f7490aee7840a28ea81e2228cfd85f6022eb189a
SouLeo <slwanna@utexas.edu>
2017-11-29 15:22:39 -0600

    untested cleaning up

--- sia5_hri_fsm/src/FSM.py
+++ sia5_hri_fsm/src/FSM.py
@@ -17,7 +17,7 @@ possiblePatterns = []
 
 class Observe(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['examine_puzzle'], input_keys=['is_block_placed_in', 'is_pattern_known'], output_keys=[''])
+        smach.State.__init__(self, outcomes=['examine_puzzle'], input_keys=['is_block_placed_in'], output_keys=[''])
 
     def execute(self, userdata):
         rospy.loginfo('Executing Observation State')
@@ -34,32 +34,36 @@ class Observe(smach.State):
 
 class ExaminePuzzle(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['give_next_block'], input_keys=['is_blocked_placed_out'], output_keys=['is_pattern_known', 'next_block'])
+        smach.State.__init__(self, outcomes=['give_next_block'], input_keys=['is_blocked_placed_out', 'is_pattern_known'], output_keys=['next_block'])
 
     def execute(self, userdata):
         rospy.loginfo('Executing Examine Puzzle State')
         # TODO: rosservice call that reports block sequence into a string and
         # report into currentPattern
         currentPattern = 'rr'   # example pattern. replace with a rosservice that determines what blocks are shown
-        global possiblePatterns
-        possiblePatterns = comparePattern(currentPattern, possiblePatterns)
-        numSolns = len(possiblePatterns)
-        userdata.next_block = nextBlock(possiblePatterns, currentPattern) # Write the next likely block
-        # TODO: check to see if user has a block in his/her workspace. i.e.
-        # do we need to go to give block?
-        if (numSolns == 1):
-            rospy.loginfo('Pattern Known!! :D')
-            userdata.is_pattern_known = True
+        if not userdata.is_pattern_known:
+            global possiblePatterns
+            possiblePatterns = comparePattern(currentPattern, possiblePatterns)
+            numSolns = len(possiblePatterns)
+            # TODO: check to see if user has a block in his/her workspace. i.e.
+            # do we need to go to give block?
+            if (numSolns == 1):
+                rospy.loginfo('Pattern Discovered!')
+                userdata.is_pattern_known = True
+            else:
+                rospy.loginfo('Pattern still unknown. ' + str(numSolns) + ' possibilities')
         else:
-            rospy.loginfo('Pattern still unknown. ' + str(numSolns) + ' possibilities')
+            rospy.loginfo('Pattern is known!')
+        userdata.next_block = nextBlock(possiblePatterns, currentPattern) # Write the next likely block
         return 'give_next_block'
+
 
 class GiveBlock(smach.State):
     # TODO: this state involves placing a block in the handover zone, the robot,
     # this may turn into a service state type or an action state type. Talk w/
     # Christina
     def __init__(self):
-        smach.State.__init__(self, outcomes=['observe'], input_keys=['is_pattern_known', 'next_block'], output_keys=['is_block_placed_in'])
+        smach.State.__init__(self, outcomes=['observe'], input_keys=['next_block'], output_keys=['is_block_placed_in'])
 
     def execute(self, userdata):
         rospy.loginfo('Executing Give Block State')
@@ -91,7 +95,7 @@ def main():
    
     with sm:
         # Add states to container
-        smach.StateMachine.add('OBSERVE', Observe(), transitions={'examine_puzzle':'EXAMINEPUZZLE'}, remapping={'is_block_placed_in':'sm_is_block_placed', 'is_pattern_known':'sm_is_pattern_known'})
+        smach.StateMachine.add('OBSERVE', Observe(), transitions={'examine_puzzle':'EXAMINEPUZZLE'}, remapping={'is_block_placed_in':'sm_is_block_placed'})
         smach.StateMachine.add('EXAMINEPUZZLE', ExaminePuzzle(), transitions={'give_next_block':'GIVEBLOCK'}, remapping={'is_block_placed_out':'sm_is_block_placed','is_pattern_known':'sm_is_pattern_known', 'next_block':'sm_next_block' })
         smach.StateMachine.add('GIVEBLOCK', GiveBlock(), transitions={'observe':'OBSERVE'}, remapping={'is_pattern_known':'sm_is_pattern_known','next_block':'sm_next_block', 'is_block_placed_in':'sm_is_block_placed'})
 
